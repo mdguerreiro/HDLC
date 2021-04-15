e327226aec6791e33b43dbd0e1f4d04a89c6c7d3
TiagoDias97 <tiagomiguelsoaresdias@gmail.com>
2021-04-15 10:48:22 +0100

    LocationReport and LocationProofReply in server implement serializable

--- location_server/src/main/java/org/acme/getting/started/LocationProofReply.java
+++ location_server/src/main/java/org/acme/getting/started/LocationProofReply.java
@@ -1,16 +1,19 @@
 package org.acme.getting.started;
+import java.io.Serializable;
 
-public class LocationProofReply {
+public class LocationProofReply implements Serializable{
 
     public String status;
     public String signer;
+    public byte[] signature;
 
     public LocationProofReply(){
     }
 
-    public LocationProofReply(String status, String signer){
+    public LocationProofReply(String status, String signer, byte[] signature){
         this.status = status;
         this.signer = signer;
+        this.signature = signature;
     }
 
 }
