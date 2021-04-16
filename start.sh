f80d94cef5215d2fd3d995a9b9938b92f35147b8
Miguel de Oliveira Guerreiro <miguel.guerreiro@talkdesk.com>
2021-04-16 14:44:29 +0000

    Fix demo file

--- start.sh
+++ start.sh
@@ -1,6 +1,11 @@
-export BYZANTINE_USERS=1 && cd location_server && ./mvnw compile quarkus:dev &
-export USERNAME="user1" && export PORT="8091" && export isByzantine="true" && cd user_node && ./mvnw compile quarkus:dev &
-export USERNAME="user2" && export PORT="8092" && export isByzantine="false" && cd user_node && ./mvnw compile quarkus:dev &
-export USERNAME="user3" && export PORT="8093" && export isByzantine="false" && cd user_node && ./mvnw compile quarkus:dev &
-export USERNAME="user4" && export PORT="8094" && export isByzantine="false" && cd user_node && ./mvnw compile quarkus:dev &
-export USERNAME="user5" && export PORT="8095" && export isByzantine="false" && cd user_node && ./mvnw compile quarkus:dev &
+export BYZANTINE_USERS=1 && cd location_server && ./mvnw compile quarkus:dev -DdebugHost=0.0.0.0 &
+sleep 1
+export USERNAME="user1" && export PORT="8091" && export isByzantine="true" && cd user_node && ./mvnw compile quarkus:dev -DdebugHost=0.0.0.0 &
+sleep 1
+export USERNAME="user2" && export PORT="8092" && export isByzantine="false" && cd user_node && ./mvnw compile quarkus:dev -DdebugHost=0.0.0.0 &
+sleep 1
+export USERNAME="user3" && export PORT="8093" && export isByzantine="false" && cd user_node && ./mvnw compile quarkus:dev -DdebugHost=0.0.0.0 &
+sleep 1
+export USERNAME="user4" && export PORT="8094" && export isByzantine="false" && cd user_node && ./mvnw compile quarkus:dev -DdebugHost=0.0.0.0 &
+sleep 1
+export USERNAME="user5" && export PORT="8095" && export isByzantine="false" && cd user_node && ./mvnw compile quarkus:dev -DdebugHost=0.0.0.0 &
