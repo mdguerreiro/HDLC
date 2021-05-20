a9e007dbe346dd6d24ffd0889b4086845ad84d8f
TiagoDias97 <tiagomiguelsoaresdias@gmail.com>
2021-05-20 23:20:57 +0100

    stuff

--- ha_client/src/main/java/org/ha/getting/started/HealthAuthorityService.java
+++ ha_client/src/main/java/org/ha/getting/started/HealthAuthorityService.java
@@ -2,6 +2,7 @@ package org.ha.getting.started;
 import org.ha.getting.started.model.*;
 import org.ha.crypto.SignatureService;
 import org.ha.getting.started.model.ObtainLocationRequest;
+import org.ha.getting.started.model.ObtainUserAtLocationRequest;
 
 import javax.inject.Inject;
 
@@ -13,6 +14,7 @@ import org.jboss.logging.Logger;
 
 
 import java.util.Base64;
+
 
 class HealthAuthorityService{
 
@@ -37,7 +39,15 @@ class HealthAuthorityService{
         }
         catch(Exception e){
             //
+            haPrivateKey = null;
+            serverPubKey = null;
+            LOG.info("ERROR LOADING KEYS");
         }
+    }
+
+
+    public HealthAuthorityService(){
+        this("user5", "location_server_8080");
     }
 
      public void signObtainLocationRequest(ObtainLocationRequest request) throws Exception{
@@ -56,6 +66,67 @@ class HealthAuthorityService{
 
      }
 
+
+    public void signObtainUserAtLocationRequest(ObtainUserAtLocationRequest request) throws Exception{
+
+        Signature signature = Signature.getInstance("SHA256withRSA");
+        signature.initSign(haPrivateKey);
+
+        signature.update(String.valueOf(request.getX()).getBytes(StandardCharsets.UTF_8));
+        signature.update(String.valueOf(request.getY()).getBytes(StandardCharsets.UTF_8));
+        signature.update(String.valueOf(request.getNonce()).getBytes(StandardCharsets.UTF_8));
+        signature.update(request.getHaId().getBytes(StandardCharsets.UTF_8));
+
+
+
+        byte[] signatureByteArray = signature.sign();
+        request.setSignature(Base64.getEncoder().encodeToString(signatureByteArray));
+
+    }
+
+
+     public boolean verifySignature(ObtainLocationRequest request) throws Exception{
+
+        String haId = "user5";
+        PublicKey haPublicKey = signatureService.getPublicKeyFromKeystore(haId);
+
+        Signature signature = Signature.getInstance("SHA256withRSA");
+        signature.initVerify(haPublicKey);
+
+        signature.update(request.getUserId().getBytes(StandardCharsets.UTF_8));
+        signature.update(String.valueOf(request.getEpoch()).getBytes(StandardCharsets.UTF_8));
+        signature.update(request.getHaId().getBytes(StandardCharsets.UTF_8));
+
+
+        byte[] requestSignature = Base64.getDecoder().decode( request.getHaSignature() );
+        boolean isValid = signature.verify(requestSignature);
+
+        return isValid;
+
+
+     }
+
+    public boolean verifySignature(ObtainUserAtLocationRequest request) throws Exception{
+
+        String haId = "user5";
+        PublicKey haPublicKey = signatureService.getPublicKeyFromKeystore(haId);
+
+        Signature signature = Signature.getInstance("SHA256withRSA");
+        signature.initVerify(haPublicKey);
+
+        signature.update(String.valueOf(request.getX()).getBytes(StandardCharsets.UTF_8));
+        signature.update(String.valueOf(request.getY()).getBytes(StandardCharsets.UTF_8));
+        signature.update(String.valueOf(request.getNonce()).getBytes(StandardCharsets.UTF_8));
+        signature.update(request.getHaId().getBytes(StandardCharsets.UTF_8));
+
+
+        byte[] requestSignature = Base64.getDecoder().decode( request.getHaSignature() );
+        boolean isValid = signature.verify(requestSignature);
+
+        return isValid;
+
+
+    }
 
 
 
--- ha_client/src/main/java/org/ha/getting/started/model/ObtainUserAtLocationRequest.java
+++ ha_client/src/main/java/org/ha/getting/started/model/ObtainUserAtLocationRequest.java
@@ -2,7 +2,7 @@ package org.ha.getting.started.model;
 import java.io.Serializable;
 
 
-class ObtainUsersAtLocationRequest implements Serializable {
+public class ObtainUserAtLocationRequest implements Serializable {
 
     int x;
     int y;
@@ -11,7 +11,7 @@ class ObtainUsersAtLocationRequest implements Serializable {
     String haSignature;
 
 
-    public ObtainUsersAtLocationRequest(int x, int y,int nonce,  String haId, String haSignature){
+    public ObtainUserAtLocationRequest(int x, int y,int nonce,  String haId, String haSignature){
 
         this.x = x;
         this.y = y;
@@ -21,5 +21,27 @@ class ObtainUsersAtLocationRequest implements Serializable {
     }
 
 
+    public int getX(){
+        return x;
+    }
 
+    public int getY(){
+        return y;
+    }
+
+    public int getNonce(){
+        return nonce;
+    }
+
+    public String getHaId(){
+        return haId;
+    }
+
+    public String getHaSignature(){
+        return haSignature;
+    }
+
+    public void setSignature(String b64Signature){
+        this.haSignature = b64Signature;
+    }
 }
